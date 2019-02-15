4ff3e62baa7307007e3c3d2959ea94b02f52079c
Ugo <ugo.vrlt26@gmail.com>
2019-02-15 23:10:31 +0100

    Add WebApp

---
+++ precache-manifest.a157d86d39d3707d7c94d5dfb2d51556.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4de3acc0cef915a4096de9ba2336f55",
+    "url": "/octogone/static/media/slide_two.f4de3acc.jpg"
+  },
+  {
+    "revision": "1c58ef4a80bb21d0e9def25d69be970e",
+    "url": "/octogone/static/media/slide_three.1c58ef4a.jpg"
+  },
+  {
+    "revision": "d63c86edc8509200e62dd52d5fe06ad3",
+    "url": "/octogone/static/media/slide_one.d63c86ed.jpg"
+  },
+  {
+    "revision": "d52415a37ac06f13e206",
+    "url": "/octogone/static/js/runtime~main.d52415a3.js"
+  },
+  {
+    "revision": "5919e3507cf2bf015793",
+    "url": "/octogone/static/js/main.5919e350.chunk.js"
+  },
+  {
+    "revision": "6cab392745b6e6a94bed",
+    "url": "/octogone/static/js/1.6cab3927.chunk.js"
+  },
+  {
+    "revision": "5919e3507cf2bf015793",
+    "url": "/octogone/static/css/main.8c2f8042.chunk.css"
+  },
+  {
+    "revision": "d48d520ff21905c588c76120d72884ac",
+    "url": "/octogone/index.html"
+  }
+];
